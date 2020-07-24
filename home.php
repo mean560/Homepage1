b8d91a522b242bd3ce5f5161df0391c711edb60b
wissutam <wissutam>
2020-07-24 20:42:05 +0700

    intial commit

---
+++ home.php
@@ -0,0 +1,68 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <link rel="stylesheet" href="homecss.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+
+</head>
+
+<body>
+    <div class="header">
+        <div class="w3-bar">
+        <a href="signin.php"><button class="w3-button w3-right" type="submit" style="background:#007398;color: white;border: 2px solid #007398">Sign in</button></a>
+            <a href="register.php"> <button class="w3-button w3-right" type="submit" style="background:white;color: black; border: 2px solid #007398;">Register</button></a>
+        </div>
+
+    </div>
+    <div class="middle">
+        <aside class="left">
+        </aside>
+        <div class="content">
+            <div class="image">
+                <img src="1.jpg" vspace="10" hspace="20" style="width:12%">
+                <img src="1.jpg" vspace="10" align="right" style="width:11%">
+                <img src="1.jpg" vspace="10" hspace="180" style="width:10%">
+            </div>
+            <h1>Reading English Article web for Beginners</h1>
+            <br>
+            <div class="w3-container">
+                <p>
+                <button class="w3-button  w3-circle" style="background:#92c8ed;color:#555;height:20%;width:14%;position:relative; bottom:50x; right:-50px">
+                        <i class="far fa-copy" style="font-size:70px;"></i></button>
+                    <font style="position:relative; right:-50px;">Parts of a sentence & translate </font>
+                    <a href="searchpage.php"><button class="w3-button  w3-circle" style="background:#fdd187;color:#555;height:20%;width:14%;position:relative; bottom:50x; right:-90px">
+                        <i class="far fa-file-alt" style="font-size:70px"></i></button></a>
+                    <font style="position:relative; right:-90px;">Search</font>
+                </p>
+                <br>
+                <p>
+                    <button class="w3-button  w3-circle" style="background:#99d9b6;color:#555;height:20%;width:14%;position:relative; bottom:50x; right:-50px">
+                        <i class="fas fa-book" style="font-size:70px;"></i></button>
+                    <font style="position:relative; right:-50px;">Bibliography</font>
+                    <button class="w3-button  w3-circle" style="background:#eb8b92;color:#555;height:20%;width:14%;position:relative; bottom:50x; right:-230px">
+                        <i class="far fa-edit" style="font-size:70px;"></i></button>
+                    <font style="position:relative; right:-230px;">Note</font>
+                </p>
+            </div>
+
+
+        </div>
+        <aside class="right">
+
+            <p> <img src="5.jpg" vspace="80" style="width:100%"></p>
+        </aside>
+    </div>
+    <div class="footer">
+
+    </div>
+
+
+</body>
+
+</html>
