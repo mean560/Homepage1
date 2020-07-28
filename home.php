219fc310ab1c3bec34acb7b26988b8ce7c56aaae
wissutam <wissutam>
2020-07-28 23:21:31 +0700

    intial commit

--- home.php
+++ home.php
@@ -3,66 +3,102 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Home</title>
 
     <link rel="stylesheet" href="homecss.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
-    <div class="header">
-        <div class="w3-bar">
-        <a href="signin.php"><button class="w3-button w3-right" type="submit" style="background:#007398;color: white;border: 2px solid #007398">Sign in</button></a>
-            <a href="register.php"> <button class="w3-button w3-right" type="submit" style="background:white;color: black; border: 2px solid #007398;">Register</button></a>
-        </div>
-
-    </div>
-    <div class="middle">
-        <aside class="left">
-        </aside>
-        <div class="content">
-            <div class="image">
-                <img src="1.jpg" vspace="10" hspace="20" style="width:12%">
-                <img src="1.jpg" vspace="10" align="right" style="width:11%">
-                <img src="1.jpg" vspace="10" hspace="180" style="width:10%">
+    <div class="wrapper">
+        <div class="header">
+            <div class="container-fluid">
+                <div class="w3-bar">
+                    <a href="signin.php"><button class="w3-button w3-right" type="submit" style="background:#007398;color: white;border: 2px solid #007398">Sign in</button></a>
+                    <a href="register.php"> <button class="w3-button w3-right" type="submit" style="background:white;color: black; border: 2px solid #007398;">Register</button></a>
+                </div>
             </div>
-            <h1>Reading English Article web for Beginners</h1>
-            <br>
-            <div class="w3-container">
-                <p>
-                <button class="w3-button  w3-circle" style="background:#92c8ed;color:#555;height:20%;width:14%;position:relative; bottom:50x; right:-50px">
-                        <i class="far fa-copy" style="font-size:70px;"></i></button>
-                    <font style="position:relative; right:-50px;">Parts of a sentence & translate </font>
-                    <a href="searchpage.php"><button class="w3-button  w3-circle" style="background:#fdd187;color:#555;height:20%;width:14%;position:relative; bottom:50x; right:-90px">
-                        <i class="far fa-file-alt" style="font-size:70px"></i></button></a>
-                    <font style="position:relative; right:-90px;">Search</font>
-                </p>
-                <br>
-                <p>
-                    <button class="w3-button  w3-circle" style="background:#99d9b6;color:#555;height:20%;width:14%;position:relative; bottom:50x; right:-50px">
-                        <i class="fas fa-book" style="font-size:70px;"></i></button>
-                    <font style="position:relative; right:-50px;">Bibliography</font>
-                    <button class="w3-button  w3-circle" style="background:#eb8b92;color:#555;height:20%;width:14%;position:relative; bottom:50x; right:-230px">
-                        <i class="far fa-edit" style="font-size:70px;"></i></button>
-                    <font style="position:relative; right:-230px;">Note</font>
-                </p>
+        </div> <!--  header -->
+        <div class="middle">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="image">
+                            <img src="1.jpg" vspace="10" hspace="20" style="width:12%">
+                            <img src="1.jpg" vspace="10" align="right" style="width:11%">
+                            <img src="1.jpg" vspace="10" hspace="180" style="width:10%">
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <h1>Reading English Article web for Beginners</h1>
+                    </div>
+                    <div class="col-12 col-md-8">
+                        <div class="content">
+                            <div class="row">
+                                <div class="col-6">
+                                    <a href="#" class="my-link">
+                                        <span class="my-image-icon">
+                                            <i class="far fa-copy"></i>
+                                        </span>
+                                        <span class="my-image-text">
+                                            Parts of a sentence & translate
+                                        </span>
+                                    </a>
+                                </div>
+                                <div class="col-6">
+                                    <a href="#" class="my-link">
+                                        <span class="my-image-icon search">
+                                            <i class="far fa-file-alt"></i>
+                                        </span>
+                                        <span class="my-image-text">
+                                            Search
+                                        </span>
+                                    </a>
+                                </div>
+                                <div class="col-6">
+                                    <a href="#" class="my-link">
+                                        <span class="my-image-icon bibliography">
+                                            <i class="fas fa-book"></i>
+                                        </span>
+                                        <span class="my-image-text">
+                                            Bibliography
+                                        </span>
+                                    </a>
+                                </div>
+                                <div class="col-6">
+                                    <a href="#" class="my-link">
+                                        <span class="my-image-icon note">
+                                            <i class="far fa-edit"></i>
+                                        </span>
+                                        <span class="my-image-text">
+                                            Note
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                            <br>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <div class="image-right">
+                            <img src="5.jpg" vspace="80" style="width:100%">
+                        </div>
+                    </div>
+                </div>
             </div>
-
-
         </div>
-        <aside class="right">
-
-            <p> <img src="5.jpg" vspace="80" style="width:100%"></p>
-        </aside>
-    </div>
+    </div><!--  wrapper -->
     <div class="footer">
 
     </div>
-
-
 </body>
-
 </html>
